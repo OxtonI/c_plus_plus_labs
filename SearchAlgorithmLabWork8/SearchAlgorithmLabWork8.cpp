d1ffb511afa33cc9cd4ad7299be61ed246f86182
OxtonI <ivan.00kzkzkz@gmail.com>
2020-05-30 21:27:42 +0500

    Fixed sorting

--- SearchAlgorithmLabWork8/SearchAlgorithmLabWork8.cpp
+++ SearchAlgorithmLabWork8/SearchAlgorithmLabWork8.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <ctime>
+#include <random>
 
 using namespace std;
 
@@ -10,11 +11,11 @@ int Search(int* startArray, int neededValue)
 	{
 		if (startArray[i] == neededValue)
 		{
-			cout << "Значение элемента есть в массиве" << endl;
+			cout << "Значение элемента " << neededValue << " есть в массиве" << endl;
 			return startArray[i];
 		}
 	}
-	cout << "Значение элемента не найдено" << endl;
+	cout << "Значение элемента " << neededValue << " НЕ найдено" << endl;
 }
 
 int BinarySearch1(int* array, int value, int left, int right)
@@ -34,13 +35,13 @@ int BinarySearch1(int* array, int value, int left, int right)
 
 	if (mark)
 	{
-		cout << "Значение элемента есть в массиве" << endl;
+		cout << "Значение элемента " << value << " есть в массиве" << endl;
 		return mid;
 	}
 
 	else
 	{
-		cout << "Значение элемента не найдено" << endl;
+		cout << "Значение элемента " << value << " НЕ найдено" << endl;
 		return -1;
 	}
 }
@@ -50,13 +51,13 @@ int BinarySearch2(int* array, int value, int left, int right)
 	int middle = (left + right) / 2;
 	if (array[middle] == value)
 	{
-		cout << "Значение элемента есть в массиве" << endl;
+		cout << "Значение элемента " << value << " есть в массиве" << endl;
 		return middle;
 	}
 
 	if (middle == left || middle == right)
 	{
-		cout << "Значение элемента не найдено" << endl;
+		cout << "Значение элемента " << value << " НЕ найдено" << endl;
 		return -1;
 	}
 
@@ -66,51 +67,86 @@ int BinarySearch2(int* array, int value, int left, int right)
 
 void QuickSort1(int* array, int first, int last)
 {
-	int mid, count;
-	mid = array[(first + last) / 2];
-	do
+	int x = first;
+	int y = last;
+	int size = last - first + 1;
+	int mid = array[first + size / 2];
+	while (x <= y)
 	{
-		while (array[first] < mid) first++;
-		while (array[last] > mid) last--;
-		if (first <= last)
+		while (array[x] < mid) x++;
+		while (array[y] > mid) y--;
+		if (x <= y)
 		{
-			count = array[first];
-			array[first] = array[last];
-			array[last] = count;
-			first++;
-			last--;
+			int temp = array[x];
+			array[x] = array[y];
+			array[y] = temp;
+			x++;
+			y--;
 		}
 	}
-
-	while (first < last);
-	if (first < last) QuickSort1(array, first, last);
-	if (first < last) QuickSort1(array, first, last);
+	if (y > first)
+		QuickSort1(array, first, y);
+	if (x < last)
+		QuickSort1(array, x, last);
 }
 
 void QuickSort2(int* array, int first, int last)
 {
-	int mid;
-	while (first < last) {
-		mid = array[(first + last) / 2];
-		while (array[first] < mid)
-			first++;
-		while (mid < array[last])
-			last--;
-		if (first <= last)
+	struct Interval
+	{
+		int first, last;
+		Interval(int first, int last) : first(first), last(last) {}
+	};
+	vector<Interval*> intervals(1, new Interval(first, last));
+
+	while (intervals.size())
+	{
+		vector<Interval*> newIntervals;
+		for (Interval* interval : intervals)
 		{
-			int temp = array[first];
-			array[first] = array[last];
-			array[last] = temp;
-			first++;
-			last--;
+			int x = interval->first;
+			int y = interval->last;
+			int size = y - x + 1;
+			int mid = array[x + size / 2];
+			while (x <= y)
+			{
+				while (array[x] < mid) x++;
+				while (array[y] > mid) y--;
+				if (x <= y)
+				{
+					int temp = array[x];
+					array[x] = array[y];
+					array[y] = temp;
+					x++;
+					y--;
+				}
+			}
+			if (y > interval->first)
+				newIntervals.push_back(new Interval(interval->first, y));
+			if (x < interval->last)
+				newIntervals.push_back(new Interval(x, interval->last));
 		}
-		if (first < last)
-			first = first;
-		else if (first < last)
-		{
-			last = last;
-			first++;
-		}
+		intervals = newIntervals;
+	}
+}
+
+void PrintElements(int* array, int num)
+{
+	cout << "Первые " << num << " элементов: ";
+	for (int i = 0; i < num; ++i)
+	{
+		cout << array[i] << " ";
+	}
+	cout << endl;
+}
+
+void RandomArrayFill(int* array, int size, int min, int max)
+{
+	std::default_random_engine generator(time(0));
+	std::uniform_int_distribution<int> random(min, max);
+	for (int i = 0; i < size; ++i)
+	{
+		array[i] = random(generator);
 	}
 }
 
@@ -126,31 +162,38 @@ int main()
 	for (int i = 0; i < N; i++)
 	{
 		Array[i] = rand() % (maxValue - minValue + 1) + minValue;
-		cout << Array[i] << endl;
 	}
+
+	int testQuickSort1[100];
+	RandomArrayFill(testQuickSort1, 100, -10, 10);
+	PrintElements(testQuickSort1, 50);
+	QuickSort1(testQuickSort1, 0, 99);
+	PrintElements(testQuickSort1, 50);
+
+	int testQuickSort2[100];
+	RandomArrayFill(testQuickSort2, 100, -10, 10);
+	PrintElements(testQuickSort2, 50);
+	QuickSort2(testQuickSort2, 0, 99);
+	PrintElements(testQuickSort2, 50);
 
 	clock_t start, end;
 	start = clock();
 	Search(Array, 6);
 	end = clock();
-	cout << "Время поиска неотсортированного массива линейным поиском: " << (((double)end - start) / ((double)CLOCKS_PER_SEC)) << endl;
+	cout << "Время поиска неотсортированного массива линейным поиском: " << (((double)end - start) / ((double)CLOCKS_PER_SEC)) << endl << endl;
 
 	start = clock();
 	BinarySearch1(Array, 6, 0, N - 1);
 	end = clock();
-	cout << "Время поиска неотсортированного массива бинарным поиском: " << (((double)end - start) / ((double)CLOCKS_PER_SEC)) << endl;
-
-	QuickSort1(Array, 0, N - 1);
-
-	QuickSort2(Array, 0, N - 1);
+	cout << "Время поиска неотсортированного массива бинарным поиском: " << (((double)end - start) / ((double)CLOCKS_PER_SEC)) << endl << endl;
 
 	start = clock();
 	BinarySearch2(Array, 2, 0, N - 1);
 	end = clock();
-	cout << "Время поиска отсортированного массива бинарным поиском: " << (((double)end - start) / ((double)CLOCKS_PER_SEC)) << endl;
+	cout << "Время поиска отсортированного массива бинарным поиском: " << (((double)end - start) / ((double)CLOCKS_PER_SEC)) << endl << endl;
 
 	start = clock();
 	Search(Array, 6);
 	end = clock();
-	cout << "Время поиска отсортированного массива линейным поиском: " << (((double)end - start) / ((double)CLOCKS_PER_SEC)) << endl;
+	cout << "Время поиска отсортированного массива линейным поиском: " << (((double)end - start) / ((double)CLOCKS_PER_SEC)) << endl << endl;
 }
